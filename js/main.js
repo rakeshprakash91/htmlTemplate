c96be4049582b6409a99b37f036a6563108fa6a0
rakeshprakash91 <rakeshprakash91@outlook.com>
2016-06-13 16:26:47 +0530

    refractoring

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+/*To change nav color when clicked*/
 function changePage(elem) {
-    var navElems = document.querySelectorAll('nav li');
-    for (var i in navElems) {
-        navElems[i].className = "";
-    }
+	var navElems = document.querySelectorAll('nav li');
+	for (var i in navElems) {
+		navElems[i].className = "";
+	}
 
-    elem.className = "selected";
+	elem.className = "selected";
+}
+/*To prevent body scroll when modal is present*/
+function stopScroll() {
+	document.body.style.overflow = "hidden";
+	console.log('inside')
+}
+/*To enable body scroll when modal is closed*/
+function enableScroll() {
+	document.body.style.overflow = "auto";
 }
